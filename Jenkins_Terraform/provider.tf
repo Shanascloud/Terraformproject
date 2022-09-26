e49d82b407d5aad6b580f98a1dbfa0e6c9c726c1
shana <45279491+Shanascloud@users.noreply.github.com>
2022-09-26 21:39:39 +0530

    Update provider.tf

--- Jenkins_Terraform/provider.tf
+++ Jenkins_Terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAQR4BLZ3Z6GJT35MR"
-  secret_key = "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"
+  access_key = "AKIAQSDFSAR4BLZ3Z6GJT35MR"
+  secret_key = "ADSADSADA/UxpQ1DeB"
   profile    = "default"
   region     = "us-east-1"
 }
