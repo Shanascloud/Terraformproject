72999482a8b1817970e036f090c167262d63354f
shana <45279491+Shanascloud@users.noreply.github.com>
2022-09-26 21:39:55 +0530

    Update provider.tf

--- Jenkins_slave/provider.tf
+++ Jenkins_slave/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAQR4BLZ3Z6GJT35MR"
-  secret_key = "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"
+  access_key = "AKIAQR4BLDASDAZ3Z6GJT35MR"
+  secret_key = "888EfADAm2GUR3iASDADADKuxoankhXzKfwVNtIPj/UxpQ1DeB"
   profile    = "default"
   region     = "us-east-1"
 }
