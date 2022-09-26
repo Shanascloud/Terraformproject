688dbf5d87d6c422f5f8bd0f0ea42b8e55d1c31b
shana <45279491+Shanascloud@users.noreply.github.com>
2022-09-26 21:38:16 +0530

    Update provider.tf

--- EC2_Creation/provider.tf
+++ EC2_Creation/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAQR4BLZ3Z6GJT35MR"
-  secret_key = "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"
+  access_key = "AKIAQR4BLZ3SDFSZ6GJT35MR"
+  secret_key = "888Efm2GUASFAAFR3iKuxoankhXzKfwVNtIPj/UxpQ1DeB"
   profile    = "default"
   region     = "us-east-1"
 }
